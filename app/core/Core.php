994ae01201a26c501bce766871ae6ca5a824e4dd
Galtiele Barobsa de Paula <galtielebarbosa@gmail.com>
2023-05-01 18:27:07 -0300

    Primeiro commit na aplicação de testes

---
+++ app/core/Core.php
@@ -0,0 +1,10 @@
+<?php
+
+
+class Core
+{
+    public function run(){
+
+        echo "Core";
+    }
+}
---
+++ app/index.php
@@ -0,0 +1,10 @@
+<?php
+
+session_start();
+
+spl_autoload_register(function($class){
+
+    $url = "/";
+
+});
+
